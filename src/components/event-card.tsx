d422358d00cd6c0fd3e42b47aff5160e95115981
adrians90 <adriansandu9090@gmail.com>
2024-05-28 21:09:49 +0100

    Add image to EventCard component, change next config to accept images from unsplash.com

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,9 +1,14 @@
 import { EventoEvent } from "@/lib/types";
+import Image from "next/image";
 
 type EventCardProps = {
   event: EventoEvent;
 };
 
 export default function EventCard({ event }: EventCardProps) {
-  return <section>{event.name}</section>;
+  return (
+    <section className="h-[380px] w-[500px]">
+      <Image src={event.imageUrl} alt={event.name} width={500} height={280} />
+    </section>
+  );
 }
