d5337abff4ea44847e8a65ce80542939906c3b3f
adrians90 <adriansandu9090@gmail.com>
2024-05-28 17:18:44 +0100

    Make SearchForm a controlled element

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -1,8 +1,15 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function SearchForm() {
+  const [searchText, setSearchText] = useState("");
+  const router = useRouter();
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!searchText) return;
+    router.push(`/events/${searchText}`);
   };
   return (
     <form onSubmit={handleSubmit} className="w-full sm:w-[580px]">
@@ -10,6 +17,8 @@ export default function SearchForm() {
         className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10"
         placeholder="Search events in any city..."
         spellCheck={false}
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
       />
     </form>
   );
