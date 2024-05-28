7313ae9d2bafeb490393589a2f9b17c3a0fa5c3d
adrians90 <adriansandu9090@gmail.com>
2024-05-28 21:11:16 +0100

    Provide event description below image

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -9,6 +9,11 @@ export default function EventCard({ event }: EventCardProps) {
   return (
     <section className="h-[380px] w-[500px]">
       <Image src={event.imageUrl} alt={event.name} width={500} height={280} />
+      <div>
+        <h2>{event.name}</h2>
+        <p>By {event.organizerName}</p>
+        <p>{event.location}</p>
+      </div>
     </section>
   );
 }
