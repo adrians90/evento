2f3bffe7d02a240d6ed1386ada7be94d4e5e0d86
adrians90 <adriansandu9090@gmail.com>
2024-05-29 16:06:10 +0100

    Refactor data fetching logic in EventPage component

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -23,3 +23,11 @@ export async function getEvents(city: string) {
   const events: EventoEvent[] = await response.json();
   return events;
 }
+
+export async function getEvent(slug: string) {
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event = await response.json();
+  return event;
+}
