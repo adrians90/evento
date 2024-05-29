255bb5553f02a4f1acdac6119457921b99e3f891
adrians90 <adriansandu9090@gmail.com>
2024-05-29 11:50:24 +0100

    Position image component

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 type EventPageProps = {
   params: {
     slug: string;
@@ -11,5 +13,29 @@ export default async function EventPage({ params }: EventPageProps) {
   );
   const event = await response.json();
 
-  return <main>Event page</main>;
+  return (
+    <main>
+      <section className="relative h-[361px] overflow-hidden">
+        <Image
+          src={event.imageUrl}
+          className="object-cover blur-3xl z-0"
+          alt="Event background image"
+          fill
+          quality={50}
+          sizes="(max-width: 1280px) 100vw, 1280px"
+          priority
+        />
+        <div className="z-1 relative">
+          <Image
+            src={event.imageUrl}
+            alt={event.name}
+            width={300}
+            height={201}
+          />
+        </div>
+      </section>
+
+      <div></div>
+    </main>
+  );
 }
