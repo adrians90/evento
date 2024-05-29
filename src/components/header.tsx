c0f1f4561a0466b405f5d2584c1ff4bdd8452192
adrians90 <adriansandu9090@gmail.com>
2024-05-29 11:15:28 +0100

    Use cn utility function to replace clsx and tailwind merge

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,8 +3,9 @@
 import Link from "next/link";
 import Logo from "./logo";
 import { usePathname } from "next/navigation";
-import clsx from "clsx";
+
 import { motion } from "framer-motion";
+import { cn } from "@/lib/utils";
 
 const routes = [
   {
@@ -27,7 +28,7 @@ export default function Header() {
           {routes.map((route) => (
             <li
               key={route.path}
-              className={clsx(
+              className={cn(
                 "hover:text-white flex items-center relative transition",
                 {
                   "text-white": activePathname === route.path,
